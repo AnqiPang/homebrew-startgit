1ff092d7e4a712791c1933428d820471efedffa6
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-04-14 13:13:57 -0700

    Update startgit.rb

--- startgit.rb
+++ startgit.rb
@@ -2,7 +2,7 @@ class Startgit < Formula
   desc "For creating a hombrew tap"
   homepage "https://github.com/AnqiPang/startgit"
   url "https://github.com/AnqiPang/startgit/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "5ff7039a3fe384d7bac0a6e3e0732e0e5e4baf79de1c72ffb2d0e77d2a4834e3"
+  sha256 "04b443ca9efc2b49959a67ac9b7fd282667e8761086502215e2de9d28b28e743"
   license "MIT"
 
   depends_on "yq"
